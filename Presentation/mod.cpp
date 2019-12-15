0077396b0e66f93982dde0ee17396167e30823ec
Streatman <streatman@t-online.de>
2019-12-15 14:17:40 +0100

    1 (#1)

---
+++ Presentation/mod.cpp
@@ -0,0 +1,10 @@
+name = 		"IntenZPack"; 								// name 
+picture = 	"IntenZPack/Presentation/Logo.gif";			// picture in expanded description
+logoSmall = "IntenZPack/Presentation/Logo_small.tga";	// icon next to mod name when description is not expanded
+logo = 		"IntenZPack/Presentation/Logo.tga";			// logo below game menu
+logoOver = 	"IntenZPack/Presentation/Logo.tga";			// on mouse hover over logo
+tooltip = 	"Server mod for IntenZ";					// tool tip on mouse hover
+overview = 	"You are looking for immersion?"; 			// overview
+action = 	"www.DayZIntenZ.com";
+author = 	"QuickZ & Streatman";
+version = 	"2";
