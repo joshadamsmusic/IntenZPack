9b0d50617948ee9f1d470edbafc1538f7b12a9af
Streatman <streatman@t-online.de>
2020-02-11 18:27:50 +0100

    Loadingscreen fix (#11)
    * updated image
    
    * Update loadingscreen.c

--- AddOns/IntenZScripts/3_Game/Loadingscreen/loadingscreen.c
+++ AddOns/IntenZScripts/3_Game/Loadingscreen/loadingscreen.c
@@ -1,24 +1,26 @@
-modded class LoadingScreen {
+modded class LoadingScreen 
+{
 	ref TStringArray loadingscreens = 
-    {
-        "IntenZ_Images/Loadingscreens/01.paa",
-        "IntenZ_Images/Loadingscreens/02.paa",
-        "IntenZ_Images/Loadingscreens/03.paa",
-        "IntenZ_Images/Loadingscreens/04.paa",
-        "IntenZ_Images/Loadingscreens/05.paa",
-        "IntenZ_Images/Loadingscreens/06.paa",
-        "IntenZ_Images/Loadingscreens/07.paa",
-        "IntenZ_Images/Loadingscreens/08.paa",
-        "IntenZ_Images/Loadingscreens/09.paa",
-        "IntenZ_Images/Loadingscreens/10.paa",
-        "IntenZ_Images/Loadingscreens/11.paa",
-        "IntenZ_Images/Loadingscreens/12.paa",
-        "IntenZ_Images/Loadingscreens/13.paa",
-        "IntenZ_Images/Loadingscreens/14.paa",
-        "IntenZ_Images/Loadingscreens/15.paa",
-    };
+	{
+		"IntenZ_Images/Loadingscreens/01.paa",
+		"IntenZ_Images/Loadingscreens/02.paa",
+		"IntenZ_Images/Loadingscreens/03.paa",
+		"IntenZ_Images/Loadingscreens/04.paa",
+		"IntenZ_Images/Loadingscreens/05.paa",
+		//"IntenZ_Images/Loadingscreens/06.paa",
+		"IntenZ_Images/Loadingscreens/07.paa",
+		"IntenZ_Images/Loadingscreens/08.paa",
+		//"IntenZ_Images/Loadingscreens/09.paa",
+		//"IntenZ_Images/Loadingscreens/10.paa",
+		"IntenZ_Images/Loadingscreens/11.paa",
+		"IntenZ_Images/Loadingscreens/12.paa",
+		"IntenZ_Images/Loadingscreens/13.paa",
+		"IntenZ_Images/Loadingscreens/14.paa",
+		"IntenZ_Images/Loadingscreens/15.paa",
+	};
 	
-	void LoadingScreen(DayZGame game) {
+	void LoadingScreen(DayZGame game) 
+	{
 		Class.CastTo(m_ImageLogoMid, m_WidgetRoot.FindAnyWidget("ImageLogoMid"));
 		Class.CastTo(m_ImageLogoCorner, m_WidgetRoot.FindAnyWidget("ImageLogoCorner"));
 		m_ImageLogoMid.Show(false);
@@ -28,7 +30,8 @@ modded class LoadingScreen {
 		ProgressAsync.SetUserData(m_ImageBackground);
 	}
 	
-	override void Show() {
+	override void Show() 
+	{
 		string lsElement = loadingscreens.GetRandomElement();
 		//m_ImageWidgetBackground.LoadMaskTexture("IntenZ_Images/Loadingscreens/01.paa");
 		m_ImageWidgetBackground.LoadImageFile(0, lsElement);
@@ -44,7 +47,6 @@ modded class LoadingScreen {
 			{
 				m_DayZGame.GetUIManager().HideDialog();
 			}
-			
 			if ( m_DayZGame.GetMissionState() == DayZGame.MISSION_STATE_MAINMENU )
 			{
 				m_ImageWidgetBackground.Show( true );		
@@ -60,33 +62,34 @@ modded class LoadingScreen {
 			m_TextWidgetTitle.SetText( "" );
 			m_TextWidgetStatus.SetText( "" );
 		}
-		
 		ProgressAsync.SetProgressData(pText);
 		ProgressAsync.SetUserData(lIcon);
 	}	
 }
 
-modded class LoginQueueBase {
+modded class LoginQueueBase 
+{
 	ref TStringArray loadingscreens = 
-    {
-        "IntenZ_Images/Loadingscreens/01.paa",
-        "IntenZ_Images/Loadingscreens/02.paa",
-        "IntenZ_Images/Loadingscreens/03.paa",
-        "IntenZ_Images/Loadingscreens/04.paa",
-        "IntenZ_Images/Loadingscreens/05.paa",
-        "IntenZ_Images/Loadingscreens/06.paa",
-        "IntenZ_Images/Loadingscreens/07.paa",
-        "IntenZ_Images/Loadingscreens/08.paa",
-        "IntenZ_Images/Loadingscreens/09.paa",
-        "IntenZ_Images/Loadingscreens/10.paa",
-        "IntenZ_Images/Loadingscreens/11.paa",
-        "IntenZ_Images/Loadingscreens/12.paa",
-        "IntenZ_Images/Loadingscreens/13.paa",
-        "IntenZ_Images/Loadingscreens/14.paa",
-        "IntenZ_Images/Loadingscreens/15.paa",
-    };
+	{
+		"IntenZ_Images/Loadingscreens/01.paa",
+		"IntenZ_Images/Loadingscreens/02.paa",
+		"IntenZ_Images/Loadingscreens/03.paa",
+		"IntenZ_Images/Loadingscreens/04.paa",
+		"IntenZ_Images/Loadingscreens/05.paa",
+		//"IntenZ_Images/Loadingscreens/06.paa",
+		"IntenZ_Images/Loadingscreens/07.paa",
+		"IntenZ_Images/Loadingscreens/08.paa",
+		//"IntenZ_Images/Loadingscreens/09.paa",
+		//"IntenZ_Images/Loadingscreens/10.paa",
+		"IntenZ_Images/Loadingscreens/11.paa",
+		"IntenZ_Images/Loadingscreens/12.paa",
+		"IntenZ_Images/Loadingscreens/13.paa",
+		"IntenZ_Images/Loadingscreens/14.paa",
+		"IntenZ_Images/Loadingscreens/15.paa",
+	};
 	
-	override Widget Init() {
+	override Widget Init() 
+	{
 		Widget initWidget = super.Init();
 		string lsElement = loadingscreens.GetRandomElement();
 		ImageWidget imageWidgetBackground = ImageWidget.Cast( layoutRoot.FindAnyWidget("Background") );
@@ -96,27 +99,29 @@ modded class LoginQueueBase {
 	}
 }
 
-modded class LoginTimeBase {
-		ref TStringArray loadingscreens = 
-    {
-        "IntenZ_Images/Loadingscreens/01.paa",
-        "IntenZ_Images/Loadingscreens/02.paa",
-        "IntenZ_Images/Loadingscreens/03.paa",
-        "IntenZ_Images/Loadingscreens/04.paa",
-        "IntenZ_Images/Loadingscreens/05.paa",
-        "IntenZ_Images/Loadingscreens/06.paa",
-        "IntenZ_Images/Loadingscreens/07.paa",
-        "IntenZ_Images/Loadingscreens/08.paa",
-        "IntenZ_Images/Loadingscreens/09.paa",
-        "IntenZ_Images/Loadingscreens/10.paa",
-        "IntenZ_Images/Loadingscreens/11.paa",
-        "IntenZ_Images/Loadingscreens/12.paa",
-        "IntenZ_Images/Loadingscreens/13.paa",
-        "IntenZ_Images/Loadingscreens/14.paa",
-        "IntenZ_Images/Loadingscreens/15.paa",
-    };
+modded class LoginTimeBase 
+{
+	ref TStringArray loadingscreens = 
+	{
+		"IntenZ_Images/Loadingscreens/01.paa",
+		"IntenZ_Images/Loadingscreens/02.paa",
+		"IntenZ_Images/Loadingscreens/03.paa",
+		"IntenZ_Images/Loadingscreens/04.paa",
+		"IntenZ_Images/Loadingscreens/05.paa",
+		//"IntenZ_Images/Loadingscreens/06.paa",
+		"IntenZ_Images/Loadingscreens/07.paa",
+		"IntenZ_Images/Loadingscreens/08.paa",
+		//"IntenZ_Images/Loadingscreens/09.paa",
+		//"IntenZ_Images/Loadingscreens/10.paa",
+		"IntenZ_Images/Loadingscreens/11.paa",
+		"IntenZ_Images/Loadingscreens/12.paa",
+		"IntenZ_Images/Loadingscreens/13.paa",
+		"IntenZ_Images/Loadingscreens/14.paa",
+		"IntenZ_Images/Loadingscreens/15.paa",
+	};
 	
-	override Widget Init() {		
+	override Widget Init() 
+	{		
 		Widget widgetToReturn = super.Init();
 		string lsElement = loadingscreens.GetRandomElement();
 		ImageWidget imageWidgetBackground = ImageWidget.Cast( layoutRoot.FindAnyWidget("Background") );
