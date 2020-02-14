91c294f349b5b1da788dcf3fe2a8a22b403f7d2c
Landbergaren <landbergaren@gmail.com>
2020-02-15 00:34:56 +0100

    removing map stuff

--- AddOns/Intenz_Scripts/4_World/chernarusmap_open.c
+++
@@ -1,7 +0,0 @@
-modded class ChernarusMap_Open extends ItemMap {
-	override void SetActions()
-	{
-		super.SetActions();		
-		AddAction(ActionUnfoldMap);
-	}
-}
--- AddOns/Intenz_Scripts/4_World/jumptest.c
+++
@@ -1,8 +0,0 @@
-modded class PlayerBase		// modded keyword for modding existing class
-{
-	override void OnJumpStart()	// overriding existing function
-	{
-		super.OnJumpStart();	// call the original jump function so we don't break stuff
-		Print("The debuging of the mod works, yay!"); // our modded print
-	}
-}
