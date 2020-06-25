af3bfc2007d2c6af2e10ade72ceeab15fa6db5b7
Landbergaren <landbergaren@gmail.com>
2020-06-25 21:49:23 +0200

    altered isolation and absorbation of clothing

--- AddOns/Intenz_Configs/CfgVehicles/Clothing_Base/Cfg.hpp
+++ AddOns/Intenz_Configs/CfgVehicles/Clothing_Base/Cfg.hpp
@@ -8,55 +8,55 @@ class Clothing: Clothing_Base {};
 class BeanieHat_ColorBase: Clothing
 {	
 	absorbency=0.80000001;
-	heatIsolation=0.30000001;
+	heatIsolation=0.90000001;
 };
 class PumpkinHelmet: Clothing
 {
 	absorbency=0;
-	heatIsolation=1;
+	heatIsolation=0.15;
 };
 class MotoHelmet_ColorBase: Clothing
 {
 	absorbency=0;
-	heatIsolation=1;
+	heatIsolation=0.7;
 };
 class Ushanka_ColorBase: Clothing
 {
 	absorbency=0.80000001;
-	heatIsolation=0.89999998;
+	heatIsolation=1;
 };
 class DarkMotoHelmet_ColorBase: Clothing
 {
 	absorbency=0;
-	heatIsolation=1;
+	heatIsolation=0.7;
 };
 class GorkaHelmet: Clothing
 {
 	absorbency=0;
-	heatIsolation=1;
+	heatIsolation=0.4;
 };
 class FirefightersHelmet_ColorBase: Clothing
 {
 	absorbency=0;
-	heatIsolation=1;
+	heatIsolation=0.4;
 };
 class DirtBikeHelmet_ColorBase: Clothing
 {
 	absorbency=0;
-	heatIsolation=1;
+	heatIsolation=0.5;
 };
 class Mich2001Helmet: Clothing
 {
 	absorbency=0;
-	heatIsolation=0.80000001;
+	heatIsolation=0.50000001;
 };
 class QuiltedJacket_ColorBase: Clothing
 {
-	heatIsolation=0.5;
+	heatIsolation=0.9;
 	absorbency=0.40000001;
 };
 class BomberJacket_ColorBase: Clothing
 {
-	heatIsolation=0.5;
-	absorbency=0.1;
+	heatIsolation=0.65;
+	absorbency=0.2;
 };
