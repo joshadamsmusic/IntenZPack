49ba1b0c6789b696ace0696adcf4ba8dc50405e4
TheVulpus <67286904+TheVulpus@users.noreply.github.com>
2020-06-26 17:03:58 +0100

    Added retextured balistic vests (#29)
    IntenZ Vest
    Black
    White
    Grey
    Light Green
    Green
    Dark Green
    Blue
    Brown

--- AddOns/Intenz_Configs/CfgVehicles/Clothing_Base/PlateCarrierVest/Cfg.hpp
+++ AddOns/Intenz_Configs/CfgVehicles/Clothing_Base/PlateCarrierVest/Cfg.hpp
@@ -1,73 +1,137 @@
 class PlateCarrierVest: Clothing {};
+class PlateCarrier_ColorBase{};
 
 class PlateCarrierVest_IntenZ: PlateCarrierVest
 {
+	hiddenSelections[]=
+	{
+		"camoGround",
+		"camoMale",
+		"camoFemale"
+	};
 	hiddenSelectionsTextures[]=
 	{
-		"Intenz_Images\Textures\PlateCarrier\Vest\IntenZ_g_co.paa",
+		"Intenz_Images\Textures\PlateCarrier\Vest\IntenZ_co.paa",
 		"Intenz_Images\Textures\PlateCarrier\Vest\IntenZ_co.paa",
 		"Intenz_Images\Textures\PlateCarrier\Vest\IntenZ_co.paa"
 	};
 };
 class PlateCarrierVest_White: PlateCarrierVest
 {
+	hiddenSelections[]=
+	{
+		"camoGround",
+		"camoMale",
+		"camoFemale"
+	};
 	hiddenSelectionsTextures[]=
 	{
-		"Intenz_Images\Textures\PlateCarrier\Vest\White_g_co.paa",
+		"Intenz_Images\Textures\PlateCarrier\Vest\White_co.paa",
 		"Intenz_Images\Textures\PlateCarrier\Vest\White_co.paa",
 		"Intenz_Images\Textures\PlateCarrier\Vest\White_co.paa"
 	};
 };
 class PlateCarrierVest_Black: PlateCarrierVest
 {
+	hiddenSelections[]=
+	{
+		"camoGround",
+		"camoMale",
+		"camoFemale"
+	};
 	hiddenSelectionsTextures[]=
 	{
-		"Intenz_Images\Textures\PlateCarrier\Vest\Black_g_co.paa",
+		"Intenz_Images\Textures\PlateCarrier\Vest\Black_co.paa",
 		"Intenz_Images\Textures\PlateCarrier\Vest\Black_co.paa",
 		"Intenz_Images\Textures\PlateCarrier\Vest\Black_co.paa"
 	};
 };
 class PlateCarrierVest_Grey: PlateCarrierVest
 {
+	hiddenSelections[]=
+	{
+		"camoGround",
+		"camoMale",
+		"camoFemale"
+	};
 	hiddenSelectionsTextures[]=
 	{
-		"Intenz_Images\Textures\PlateCarrier\Vest\Grey_g_co.paa",
+		"Intenz_Images\Textures\PlateCarrier\Vest\Grey_co.paa",
 		"Intenz_Images\Textures\PlateCarrier\Vest\Grey_co.paa",
 		"Intenz_Images\Textures\PlateCarrier\Vest\Grey_co.paa"
 	};
 };
 class PlateCarrierVest_Blue: PlateCarrierVest
 {
+	hiddenSelections[]=
+	{
+		"camoGround",
+		"camoMale",
+		"camoFemale"
+	};
 	hiddenSelectionsTextures[]=
 	{
-		"Intenz_Images\Textures\PlateCarrier\Vest\Blue_g_co.paa",
+		"Intenz_Images\Textures\PlateCarrier\Vest\Blue_co.paa",
 		"Intenz_Images\Textures\PlateCarrier\Vest\Blue_co.paa",
 		"Intenz_Images\Textures\PlateCarrier\Vest\Blue_co.paa"
 	};
 };
-class PlateCarrierVest_Green: PlateCarrierVest
+class PlateCarrierVest_LightGreen: PlateCarrierVest
 {
+	hiddenSelections[]=
+	{
+		"camoGround",
+		"camoMale",
+		"camoFemale"
+	};
 	hiddenSelectionsTextures[]=
 	{
-		"Intenz_Images\Textures\PlateCarrier\Vest\Green_g_co.paa",
+		"Intenz_Images\Textures\PlateCarrier\Vest\Light_Green_co.paa",
+		"Intenz_Images\Textures\PlateCarrier\Vest\Light_Green_co.paa",
+		"Intenz_Images\Textures\PlateCarrier\Vest\Light_Green_co.paa"
+	};
+};
+class PlateCarrierVest_Green: PlateCarrierVest
+{
+	hiddenSelections[]=
+	{
+		"camoGround",
+		"camoMale",
+		"camoFemale"
+	};
+	hiddenSelectionsTextures[]=
+	{
+		"Intenz_Images\Textures\PlateCarrier\Vest\Green_co.paa",
 		"Intenz_Images\Textures\PlateCarrier\Vest\Green_co.paa",
 		"Intenz_Images\Textures\PlateCarrier\Vest\Green_co.paa"
 	};
 };
 class PlateCarrierVest_Dark_Green: PlateCarrierVest
 {
+	hiddenSelections[]=
+	{
+		"camoGround",
+		"camoMale",
+		"camoFemale"
+	};
 	hiddenSelectionsTextures[]=
 	{
-		"Intenz_Images\Textures\PlateCarrier\Vest\Dark_Green_g_co.paa",
+		"Intenz_Images\Textures\PlateCarrier\Vest\Dark_Green_co.paa",
 		"Intenz_Images\Textures\PlateCarrier\Vest\Dark_Green_co.paa",
 		"Intenz_Images\Textures\PlateCarrier\Vest\Dark_Green_co.paa"
 	};
 };
 class PlateCarrierVest_Brown: PlateCarrierVest
 {
+	hiddenSelections[]=
+	{
+		"camoGround",
+		"camoMale",
+		"camoFemale"
+	};
 	hiddenSelectionsTextures[]=
 	{
-		"Intenz_Images\Textures\PlateCarrier\Vest\Brown_g_co.paa",
+		"Intenz_Images\Textures\PlateCarrier\Vest\Brown_co.paa",
 		"Intenz_Images\Textures\PlateCarrier\Vest\Brown_co.paa",
 		"Intenz_Images\Textures\PlateCarrier\Vest\Brown_co.paa"
 	};
