9889edece7c990fe0fa7b978d4fd09c4b8c1bd8f
TheVulpus <67286904+TheVulpus@users.noreply.github.com>
2020-06-28 08:07:57 +0100

    25 new armbands and recipes from base game jackets and tops (#37)

--- AddOns/Intenz_Configs/CfgVehicles/Clothing_Base/Armband_ColorBase/Cfg.hpp
+++ AddOns/Intenz_Configs/CfgVehicles/Clothing_Base/Armband_ColorBase/Cfg.hpp
@@ -15,3 +15,378 @@ class Intenz_Armband: Armband_ColorBase
 		"Intenz_Images\Textures\Armbands\IntenzArmband_co.paa"
 	};
 };
+class CheckBlue_Armband: Armband_ColorBase
+{
+	displayName="Blue Checkered Armband";
+	descriptionShort="Armband created by cutting up a blue checkered shirt";
+
+	scope=2;
+	hiddenSelectionsTextures[]=
+	{
+		"Intenz_Images\Textures\Armbands\Checkblue_g_co.paa",
+		"Intenz_Images\Textures\Armbands\Checkblue_co.paa",
+		"Intenz_Images\Textures\Armbands\Checkblue_co.paa",
+		"Intenz_Images\Textures\Armbands\Checkblue_co.paa",
+		"Intenz_Images\Textures\Armbands\Checkblue_co.paa"
+	};
+};
+class CheckDarkBlue_Armband: Armband_ColorBase
+{
+	displayName="Dark Blue Checkered Armband";
+	descriptionShort="Armband created by cutting up a dark blue checkered shirt";
+
+	scope=2;
+	hiddenSelectionsTextures[]=
+	{
+		"Intenz_Images\Textures\Armbands\Checkdarkblue_g_co.paa",
+		"Intenz_Images\Textures\Armbands\Checkdarkblue_co.paa",
+		"Intenz_Images\Textures\Armbands\Checkdarkblue_co.paa",
+		"Intenz_Images\Textures\Armbands\Checkdarkblue_co.paa",
+		"Intenz_Images\Textures\Armbands\Checkdarkblue_co.paa"
+	};
+};
+class CheckGreen_Armband: Armband_ColorBase
+{
+	displayName="Green Checkered Armband";
+	descriptionShort="Armband created by cutting up a green checkered shirt";
+
+	scope=2;
+	hiddenSelectionsTextures[]=
+	{
+		"Intenz_Images\Textures\Armbands\CheckGreen_g_co.paa",
+		"Intenz_Images\Textures\Armbands\CheckGreen_co.paa",
+		"Intenz_Images\Textures\Armbands\CheckGreen_co.paa",
+		"Intenz_Images\Textures\Armbands\CheckGreen_co.paa",
+		"Intenz_Images\Textures\Armbands\CheckGreen_co.paa"
+	};
+};
+class CheckRed_Armband: Armband_ColorBase
+{
+	displayName="Red Checkered Armband";
+	descriptionShort="Armband created by cutting up a red checkered shirt";
+
+	scope=2;
+	hiddenSelectionsTextures[]=
+	{
+		"Intenz_Images\Textures\Armbands\CheckRed_g_co.paa",
+		"Intenz_Images\Textures\Armbands\CheckRed_co.paa",
+		"Intenz_Images\Textures\Armbands\CheckRed_co.paa",
+		"Intenz_Images\Textures\Armbands\CheckRed_co.paa",
+		"Intenz_Images\Textures\Armbands\CheckRed_co.paa"
+	};
+};
+class CheckWhite_Armband: Armband_ColorBase
+{
+	displayName="White Checkered Armband";
+	descriptionShort="Armband created by cutting up a white checkered shirt";
+
+	scope=2;
+	hiddenSelectionsTextures[]=
+	{
+		"Intenz_Images\Textures\Armbands\Checkwhite_g_co.paa",
+		"Intenz_Images\Textures\Armbands\Checkwhite_co.paa",
+		"Intenz_Images\Textures\Armbands\Checkwhite_co.paa",
+		"Intenz_Images\Textures\Armbands\Checkwhite_co.paa",
+		"Intenz_Images\Textures\Armbands\Checkwhite_co.paa"
+	};
+};
+class FireFighterBeige_Armband: Armband_ColorBase
+{
+	displayName="Beige Firefighter Armband";
+	descriptionShort="Armband created by cutting up a beige firefighter jacket";
+
+	scope=2;
+	hiddenSelectionsTextures[]=
+	{
+		"Intenz_Images\Textures\Armbands\FireFighterBeige_g_co.paa",
+		"Intenz_Images\Textures\Armbands\FireFighterBeige_co.paa",
+		"Intenz_Images\Textures\Armbands\FireFighterBeige_co.paa",
+		"Intenz_Images\Textures\Armbands\FireFighterBeige_co.paa",
+		"Intenz_Images\Textures\Armbands\FireFighterBeige_co.paa"
+	};
+};
+class FireFighterBlack_Armband: Armband_ColorBase
+{
+	displayName="Black Firefighter Armband";
+	descriptionShort="Armband created by cutting up a black firefighter jacket";
+
+	scope=2;
+	hiddenSelectionsTextures[]=
+	{
+		"Intenz_Images\Textures\Armbands\FireFighterblack_g_co.paa",
+		"Intenz_Images\Textures\Armbands\FireFighterblack_co.paa",
+		"Intenz_Images\Textures\Armbands\FireFighterblack_co.paa",
+		"Intenz_Images\Textures\Armbands\FireFighterblack_co.paa",
+		"Intenz_Images\Textures\Armbands\FireFighterblack_co.paa"
+	};
+};
+class NurseBlue_Armband: Armband_ColorBase
+{
+	displayName="Blue Nurse Armband";
+	descriptionShort="Armband created by cutting up a blue Nurses dress";
+
+	scope=2;
+	hiddenSelectionsTextures[]=
+	{
+		"Intenz_Images\Textures\Armbands\NurseBlue_g_co.paa",
+		"Intenz_Images\Textures\Armbands\NurseBlue_co.paa",
+		"Intenz_Images\Textures\Armbands\NurseBlue_co.paa",
+		"Intenz_Images\Textures\Armbands\NurseBlue_co.paa",
+		"Intenz_Images\Textures\Armbands\NurseBlue_co.paa"
+	};
+};
+class NurseWhite_Armband: Armband_ColorBase
+{
+	displayName="White Nurse Armband";
+	descriptionShort="Armband created by cutting up a white Nurses dress";
+
+	scope=2;
+	hiddenSelectionsTextures[]=
+	{
+		"Intenz_Images\Textures\Armbands\NurseWhite_g_co.paa",
+		"Intenz_Images\Textures\Armbands\NurseWhite_co.paa",
+		"Intenz_Images\Textures\Armbands\NurseWhite_co.paa",
+		"Intenz_Images\Textures\Armbands\NurseWhite_co.paa",
+		"Intenz_Images\Textures\Armbands\NurseWhite_co.paa"
+	};
+};
+class ParamedicBlue_Armband: Armband_ColorBase
+{
+	displayName="Blue Paramedic Armband";
+	descriptionShort="Armband created by cutting up a blue paramedic jacket";
+
+	scope=2;
+	hiddenSelectionsTextures[]=
+	{
+		"Intenz_Images\Textures\Armbands\ParamedicBlue_g_co.paa",
+		"Intenz_Images\Textures\Armbands\ParamedicBlue_co.paa",
+		"Intenz_Images\Textures\Armbands\ParamedicBlue_co.paa",
+		"Intenz_Images\Textures\Armbands\ParamedicBlue_co.paa",
+		"Intenz_Images\Textures\Armbands\ParamedicBlue_co.paa"
+	};
+};
+class ParamedicGreen_Armband: Armband_ColorBase
+{
+	displayName="Green Paramedic Armband";
+	descriptionShort="Armband created by cutting up a green paramedic jacket";
+
+	scope=2;
+	hiddenSelectionsTextures[]=
+	{
+		"Intenz_Images\Textures\Armbands\ParamedicGreen_g_co.paa",
+		"Intenz_Images\Textures\Armbands\ParamedicGreen_co.paa",
+		"Intenz_Images\Textures\Armbands\ParamedicGreen_co.paa",
+		"Intenz_Images\Textures\Armbands\ParamedicGreen_co.paa",
+		"Intenz_Images\Textures\Armbands\ParamedicGreen_co.paa"
+	};
+};
+class ParamedicRed_Armband: Armband_ColorBase
+{
+	displayName="Red Paramedic Armband";
+	descriptionShort="Armband created by cutting up a red paramedic jacket";
+
+	scope=2;
+	hiddenSelectionsTextures[]=
+	{
+		"Intenz_Images\Textures\Armbands\ParamedicRed_g_co.paa",
+		"Intenz_Images\Textures\Armbands\ParamedicRed_co.paa",
+		"Intenz_Images\Textures\Armbands\ParamedicRed_co.paa",
+		"Intenz_Images\Textures\Armbands\ParamedicRed_co.paa",
+		"Intenz_Images\Textures\Armbands\ParamedicRed_co.paa"
+	};
+};
+class Police_Armband: Armband_ColorBase
+{
+	displayName="Police Armband";
+	descriptionShort="Armband created by cutting up a police jacket";
+
+	scope=2;
+	hiddenSelectionsTextures[]=
+	{
+		"Intenz_Images\Textures\Armbands\Police_g_co.paa",
+		"Intenz_Images\Textures\Armbands\Police_co.paa",
+		"Intenz_Images\Textures\Armbands\Police_co.paa",
+		"Intenz_Images\Textures\Armbands\Police_co.paa",
+		"Intenz_Images\Textures\Armbands\Police_co.paa"
+	};
+};
+class PoliceKamy_Armband: Armband_ColorBase
+{
+	displayName="Police Armband";
+	descriptionShort="Armband created by cutting up a police jacket";
+
+	scope=2;
+	hiddenSelectionsTextures[]=
+	{
+		"Intenz_Images\Textures\Armbands\PoliceKamy_g_co.paa",
+		"Intenz_Images\Textures\Armbands\PoliceKamy_co.paa",
+		"Intenz_Images\Textures\Armbands\PoliceKamy_co.paa",
+		"Intenz_Images\Textures\Armbands\PoliceKamy_co.paa",
+		"Intenz_Images\Textures\Armbands\PoliceKamy_co.paa"
+	};
+};
+class Prison_Armband: Armband_ColorBase
+{
+	displayName="Prison Armband";
+	descriptionShort="Armband created by cutting up a prison jacket";
+
+	scope=2;
+	hiddenSelectionsTextures[]=
+	{
+		"Intenz_Images\Textures\Armbands\Prison_g_co.paa",
+		"Intenz_Images\Textures\Armbands\Prison_co.paa",
+		"Intenz_Images\Textures\Armbands\Prison_co.paa",
+		"Intenz_Images\Textures\Armbands\Prison_co.paa",
+		"Intenz_Images\Textures\Armbands\Prison_co.paa"
+	};
+};
+class ScrubsBlue_Armband: Armband_ColorBase
+{
+	displayName="Blue Scrubs Armband";
+	descriptionShort="Armband created by cutting up a blue scrub top";
+
+	scope=2;
+	hiddenSelectionsTextures[]=
+	{
+		"Intenz_Images\Textures\Armbands\ScrubsBlue_g_co.paa",
+		"Intenz_Images\Textures\Armbands\ScrubsBlue_co.paa",
+		"Intenz_Images\Textures\Armbands\ScrubsBlue_co.paa",
+		"Intenz_Images\Textures\Armbands\ScrubsBlue_co.paa",
+		"Intenz_Images\Textures\Armbands\ScrubsBlue_co.paa"
+	};
+};
+class ScrubsGreen_Armband: Armband_ColorBase
+{
+	displayName="Green Scrubs Armband";
+	descriptionShort="Armband created by cutting up a green scrub top";
+
+	scope=2;
+	hiddenSelectionsTextures[]=
+	{
+		"Intenz_Images\Textures\Armbands\ScrubsGreen_g_co.paa",
+		"Intenz_Images\Textures\Armbands\ScrubsGreen_co.paa",
+		"Intenz_Images\Textures\Armbands\ScrubsGreen_co.paa",
+		"Intenz_Images\Textures\Armbands\ScrubsGreen_co.paa",
+		"Intenz_Images\Textures\Armbands\ScrubsGreen_co.paa"
+	};
+};
+class TShirtBeige_Armband: Armband_ColorBase
+{
+	displayName="Beige TShirt Armband";
+	descriptionShort="Armband created by cutting up a beige t-shirt";
+
+	scope=2;
+	hiddenSelectionsTextures[]=
+	{
+		"Intenz_Images\Textures\Armbands\tshirt_beige_g_co.paa",
+		"Intenz_Images\Textures\Armbands\tshirt_beige_co.paa",
+		"Intenz_Images\Textures\Armbands\tshirt_beige_co.paa",
+		"Intenz_Images\Textures\Armbands\tshirt_beige_co.paa",
+		"Intenz_Images\Textures\Armbands\tshirt_beige_co.paa"
+	};
+};
+class TShirtBlack_Armband: Armband_ColorBase
+{
+	displayName="Black TShirt Armband";
+	descriptionShort="Armband created by cutting up a black t-shirt";
+
+	scope=2;
+	hiddenSelectionsTextures[]=
+	{
+		"Intenz_Images\Textures\Armbands\tshirt_black_g_co.paa",
+		"Intenz_Images\Textures\Armbands\tshirt_black_co.paa",
+		"Intenz_Images\Textures\Armbands\tshirt_black_co.paa",
+		"Intenz_Images\Textures\Armbands\tshirt_black_co.paa",
+		"Intenz_Images\Textures\Armbands\tshirt_black_co.paa"
+	};
+};
+class TShirtBlue_Armband: Armband_ColorBase
+{
+	displayName="Blue TShirt Armband";
+	descriptionShort="Armband created by cutting up a blue t-shirt";
+
+	scope=2;
+	hiddenSelectionsTextures[]=
+	{
+		"Intenz_Images\Textures\Armbands\tshirt_blue_g_co.paa",
+		"Intenz_Images\Textures\Armbands\tshirt_blue_co.paa",
+		"Intenz_Images\Textures\Armbands\tshirt_blue_co.paa",
+		"Intenz_Images\Textures\Armbands\tshirt_blue_co.paa",
+		"Intenz_Images\Textures\Armbands\tshirt_blue_co.paa"
+	};
+};
+class TShirtGreen_Armband: Armband_ColorBase
+{
+	displayName="Green TShirt Armband";
+	descriptionShort="Armband created by cutting up a green t-shirt";
+
+	scope=2;
+	hiddenSelectionsTextures[]=
+	{
+		"Intenz_Images\Textures\Armbands\tshirt_green_g_co.paa",
+		"Intenz_Images\Textures\Armbands\tshirt_green_co.paa",
+		"Intenz_Images\Textures\Armbands\tshirt_green_co.paa",
+		"Intenz_Images\Textures\Armbands\tshirt_green_co.paa",
+		"Intenz_Images\Textures\Armbands\tshirt_green_co.paa"
+	};
+};
+class TShirtGrey_Armband: Armband_ColorBase
+{
+	displayName="Grey TShirt Armband";
+	descriptionShort="Armband created by cutting up a grey t-shirt";
+
+	scope=2;
+	hiddenSelectionsTextures[]=
+	{
+		"Intenz_Images\Textures\Armbands\tshirt_grey_g_co.paa",
+		"Intenz_Images\Textures\Armbands\tshirt_grey_co.paa",
+		"Intenz_Images\Textures\Armbands\tshirt_grey_co.paa",
+		"Intenz_Images\Textures\Armbands\tshirt_grey_co.paa",
+		"Intenz_Images\Textures\Armbands\tshirt_grey_co.paa"
+	};
+};
+class TShirtRed_Armband: Armband_ColorBase
+{
+	displayName="Red TShirt Armband";
+	descriptionShort="Armband created by cutting up a red t-shirt";
+
+	scope=2;
+	hiddenSelectionsTextures[]=
+	{
+		"Intenz_Images\Textures\Armbands\tshirt_red_g_co.paa",
+		"Intenz_Images\Textures\Armbands\tshirt_red_co.paa",
+		"Intenz_Images\Textures\Armbands\tshirt_red_co.paa",
+		"Intenz_Images\Textures\Armbands\tshirt_red_co.paa",
+		"Intenz_Images\Textures\Armbands\tshirt_red_co.paa"
+	};
+};
+class TShirtRedStriped_Armband: Armband_ColorBase
+{
+	displayName="Red Striped TShirt Armband";
+	descriptionShort="Armband created by cutting up a red striped t-shirt";
+
+	scope=2;
+	hiddenSelectionsTextures[]=
+	{
+		"Intenz_Images\Textures\Armbands\tshirt_redblackstripes_g_co.paa",
+		"Intenz_Images\Textures\Armbands\tshirt_redblackstripes_co.paa",
+		"Intenz_Images\Textures\Armbands\tshirt_redblackstripes_co.paa",
+		"Intenz_Images\Textures\Armbands\tshirt_redblackstripes_co.paa",
+		"Intenz_Images\Textures\Armbands\tshirt_redblackstripes_co.paa"
+	};
+};
+class TShirtWhite_Armband: Armband_ColorBase
+{
+	displayName="White TShirt Armband";
+	descriptionShort="Armband created by cutting up a white striped t-shirt";
+
+	scope=2;
+	hiddenSelectionsTextures[]=
+	{
+		"Intenz_Images\Textures\Armbands\tshirt_white_g_co.paa",
+		"Intenz_Images\Textures\Armbands\tshirt_white_co.paa",
+		"Intenz_Images\Textures\Armbands\tshirt_white_co.paa",
+		"Intenz_Images\Textures\Armbands\tshirt_white_co.paa",
+		"Intenz_Images\Textures\Armbands\tshirt_white_co.paa"
+	};
+};
--- AddOns/Intenz_Scripts/4_World/recipe/RecipeManager.c
+++ AddOns/Intenz_Scripts/4_World/recipe/RecipeManager.c
@@ -6,5 +6,29 @@ modded class PluginRecipesManager
 
 		//Clothing
 		RegisterRecipe(new CraftIntenzArmband);
+		RegisterRecipe(new CraftCheckBlueArmband);
+		RegisterRecipe(new CraftCheckDarkBlueArmband);
+		RegisterRecipe(new CraftCheckGreenArmband);
+		RegisterRecipe(new CraftCheckRedArmband);
+		RegisterRecipe(new CraftCheckWhiteArmband);
+		RegisterRecipe(new CraftFirefighterBeigeArmband);
+		RegisterRecipe(new CraftFirefighterBlackArmband);
+		RegisterRecipe(new CraftNurseBlueArmband);
+		RegisterRecipe(new CraftNurseWhiteArmband);
+		RegisterRecipe(new CraftParamedicBlueArmband);
+		RegisterRecipe(new CraftParamedicGreenArmband);
+		RegisterRecipe(new CraftParamedicRedArmband);
+		RegisterRecipe(new CraftPoliceArmband);
+		RegisterRecipe(new CraftPoliceKamyArmband);
+		RegisterRecipe(new CraftPrisonArmband);
+		RegisterRecipe(new CraftScrubsBlueArmband);
+		RegisterRecipe(new CraftTshirtBeigeArmband);
+		RegisterRecipe(new CraftTshirtBlackArmband);
+		RegisterRecipe(new CraftTshirtBlueArmband);
+		RegisterRecipe(new CraftTshirtGreenArmband);
+		RegisterRecipe(new CraftTshirtGreyArmband);
+		RegisterRecipe(new CraftTshirtRedArmband);
+		RegisterRecipe(new CraftTshirtRedStripedArmband);
+		RegisterRecipe(new CraftTshirtWhiteArmband);
 	}	
 }
